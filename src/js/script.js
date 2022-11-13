521f862094509608e6c0821f18f7b3f4aab364fa
Sylvain Tagnabou <sylvaintagnabou@gmail.com>
2022-11-13 19:26:26 +0000

    t

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -10,11 +11,36 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+const orbit = new OrbitControls(camera, renderer.domElement);
+
 const axisHelper = new THREE.AxesHelper(5);
 scene.add(axisHelper);
 
-camera.position.set(0,2,5); 
+camera.position.set(-10,30,30); 
+orbit.update();
 
-const boxGeometry = new THREE.BoxGeometry(1,1,1);
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
 
-renderer.render(scene, camera);
+// Big box
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0,5 * Math.PI;
+
+//grid helper 
+const gridHelper = new THREE.GridHelper(30 );
+scene.add(gridHelper);
+
+
+function animate(time){
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+
+}
+
+renderer.setAnimationLoop(animate);
